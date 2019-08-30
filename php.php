4c60beb1353c8cde2a496e8af1521f1441da0cc4
AmirhoseinDelavar <am.delavar@gmail.com>
2019-08-30 16:20:27 +0430

    login if successed fade the form

--- php.php
+++ php.php
@@ -8,6 +8,8 @@
 </head>
 <body>
 <?php
+ini_set('display_errors', 'on');
+error_reporting(-1);
 $num0 = (int)"123ali";// 123
 $num1 = (int)"ali123";// 0
 
@@ -18,37 +20,46 @@ gettype($num0);
 //        if (isset($_GET['btn-submit']))
 //            $a = $_GET['fn'];
 //        print $a;
+$u = isset($_POST['un']) ? $_POST['un'] : '';
+$p = isset($_POST['pass']) ? $_POST['pass'] : '';
+$isShow = true;
 if (isset($_POST['btn-submit'])) {
     if ($_POST['un'] == '')
         print "enter u";
     elseif ($_POST['pass'] == '')
         print "enter pass";
-    elseif ($_POST['un'] == 'ali') {
-        if ($_POST['pass'] == '123')
-            print 'hi ali';
+    else{
+        $u = $_POST['un'];
+        $p = $_POST['pass'];
+        if (($u == 'ali' && $p == '123') || ($u == 'reza' && $p == '321'))
+            $isShow = false;
         else
-            print 'pass not correct';
-    } elseif ($_POST['un'] == 'reza') {
-        if ($_POST['pass'] == '321')
-            print 'hi reza';
-        else
-            print 'pass not correct';
+            print 'check pass and username';
+
     }
-
-
 }
 
 ?>
-<form action="" method="post">
-    <div>
-        <label for="un">un:</label>
-        <input type="text" name="un">
-        <label for="Pass">Pass:</label>
-        <input type="text" name="pass">
-    </div>
-    <div>
-        <input type="submit" name="btn-submit" value="send">
-    </div>
-</form>
+<?php
+    if ($isShow) {
+        ?>
+        <form action="" method="post">
+            <div>
+                <label for="un">un:</label>
+                <input type="text" name="un" value="<?php
+                print $u;
+                ?>">
+                <label for="Pass">Pass:</label>
+                <input type="text" name="pass" value="<?php
+                print $p;
+                ?>">
+            </div>
+            <div>
+                <input type="submit" name="btn-submit" value="send">
+            </div>
+        </form>
+        <?php
+    }
+?>
 </body>
 </html>
