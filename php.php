f200eed41ca74f5273d3bda345b7335ecd154aa3
AmirhoseinDelavar <am.delavar@gmail.com>
2019-08-30 15:33:58 +0430

    simplelogin

--- php.php
+++ php.php
@@ -7,26 +7,48 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $num0 = (int)"123ali";// 123
-        $num1 = (int)"ali123";// 0
+<?php
+$num0 = (int)"123ali";// 123
+$num1 = (int)"ali123";// 0
 
-        is_numeric($num0);
-        is_int($num0);
-        gettype($num0);
+is_numeric($num0);
+is_int($num0);
+gettype($num0);
 
-        if (isset($_GET['btn-submit']))
-            $a = $_GET['fn'];
-        print $a;
-    ?>
-    <form action="" method="get">
-        <div>
-            <label for="fn">Fn:</label>
-            <input type="text" name="fn">
-        </div>
-        <div>
-            <input type="submit" name="btn-submit" value="send">
-        </div>
-    </form>
+//        if (isset($_GET['btn-submit']))
+//            $a = $_GET['fn'];
+//        print $a;
+if (isset($_POST['btn-submit'])) {
+    if ($_POST['un'] == '')
+        print "enter u";
+    elseif ($_POST['pass'] == '')
+        print "enter pass";
+    elseif ($_POST['un'] == 'ali') {
+        if ($_POST['pass'] == '123')
+            print 'hi ali';
+        else
+            print 'pass not correct';
+    } elseif ($_POST['un'] == 'reza') {
+        if ($_POST['pass'] == '321')
+            print 'hi reza';
+        else
+            print 'pass not correct';
+    }
+
+
+}
+
+?>
+<form action="" method="post">
+    <div>
+        <label for="un">un:</label>
+        <input type="text" name="un">
+        <label for="Pass">Pass:</label>
+        <input type="text" name="pass">
+    </div>
+    <div>
+        <input type="submit" name="btn-submit" value="send">
+    </div>
+</form>
 </body>
 </html>
